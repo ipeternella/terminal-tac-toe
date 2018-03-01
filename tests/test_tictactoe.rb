ccd666e566b9b790f902878e006b631637419a18
IgooorGP <igor.feq@gmail.com>
2018-02-28 23:03:26 -0300

    Implemented game levels, different is game over tests

--- tests/test_tictactoe.rb
+++ tests/test_tictactoe.rb
@@ -46,6 +46,126 @@ class TestTicTacToe < Test::Unit::TestCase
     # after setting mark
     assert_equal(@game.is_valid_spot(2, @board), false)
   end
+
+  def test_get_empty_spaces
+    # marks spots with com_mark
+    @board[0] = @game.com_mark
+
+    # marks spots with hum_mark
+    @board[1] = @game.hum_mark
+
+    # expected available spaces
+    as = ["2", "3", "4", "5", "6", "7", "8"]
+
+    assert_equal(@game.get_available_spaces(@board), as)
+  end
+
+  # def test_get_game_difficulty
+  #   puts "testing game difficulty... type 3"
+    
+  #   assert_equal(@game.get_game_difficulty, 3)
+  # end
+
+  def test_get_com_move
+    # marks spots with com_mark
+    @board[0] = @game.com_mark
+
+    # marks spots with hum_mark
+    @board[8] = @game.hum_mark
+
+    # random_gen returns 7
+    seed = 123
+    
+    assert_equal(@game.get_com_move(@board, 1, Random.new(seed)), 7)
+
+    # marks spots with hum_mark in position 7
+    @board[7] = @game.hum_mark
+
+    assert_equal(@game.get_com_move(@board, 1, Random.new(seed)), 6)
+  end
+
+  def test_horizontal_scan_1
+    # marks spots with hum_mark
+    @board[0] = @game.hum_mark
+    @board[1] = @game.hum_mark
+    @board[2] = @game.com_mark
+
+    assert_equal(@game.horizontal_scan(@board, 3), false)
+  end
+
+  def test_horizontal_scan_2
+    # marks spots with hum_mark
+    @board[0] = @game.hum_mark
+    @board[1] = @game.hum_mark
+    @board[3] = @game.hum_mark
+
+    assert_equal(@game.horizontal_scan(@board, 3), false)
+  end
+
+  def test_horizontal_scan_3
+    # marks spots with hum_mark
+    @board[6] = @game.hum_mark
+    @board[7] = @game.hum_mark
+    @board[8] = @game.hum_mark
+
+    assert_equal(@game.horizontal_scan(@board, 3), true)
+  end
   
+  def test_vertical_scan_1
+    @board[0] = @game.hum_mark
+    @board[3] = @game.hum_mark
+    @board[6] = @game.hum_mark
+
+    assert_equal(@game.vertical_scan(@board, 3), true)
+  end
+
+  def test_vertical_scan_2
+    @board[2] = @game.hum_mark
+    @board[5] = @game.hum_mark
+    @board[8] = @game.hum_mark
+
+    assert_equal(@game.vertical_scan(@board, 3), true)
+  end
+
+  def test_vertical_scan_3
+    @board[1] = @game.hum_mark
+    @board[4] = @game.hum_mark
+    @board[7] = @game.hum_mark
+
+    assert_equal(@game.vertical_scan(@board, 3), true)
+  end
+
+  def test_vertical_scan_4
+    @board[1] = @game.hum_mark
+    @board[4] = @game.com_mark
+    @board[7] = @game.hum_mark
+
+    assert_equal(@game.vertical_scan(@board, 3), false)
+  end
+
+  def test_vertical_scan_5
+    @board[0] = @game.hum_mark
+    @board[3] = @game.hum_mark
+    @board[5] = @game.hum_mark
+
+    assert_equal(@game.vertical_scan(@board, 3), false)
+  end
+  
+
+  def test_diag_scan_1
+    @board[0] = @game.hum_mark
+    @board[4] = @game.hum_mark
+    @board[8] = @game.hum_mark
+
+    assert_equal(@game.diag_scan(@board, 3), true)
+  end
+
+  def test_diag_scan_2
+    @board[2] = @game.hum_mark
+    @board[4] = @game.hum_mark
+    @board[6] = @game.hum_mark
+
+    assert_equal(@game.diag_scan(@board, 3), true)
+  end
   
 end
--- tictactoe.rb
+++ tictactoe.rb
@@ -15,25 +15,57 @@ class Game
     @size = 3
     @com_mark = "X" # the computers marker
     @hum_mark = "O" # the users marker
+    @difficulty = nil
   end
 
   def start_game
     # main function of the game: main loop
+    @difficulty = get_game_difficulty
     
     # start by printing the board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
     puts "Enter [0-8]:"
-    
+
     # loop through until the game was won or tied
-    until game_is_over(@board) || tie(@board)
+    until is_game_over(@board, @size) || tie(@board)
+
       get_human_spot
-      if !game_is_over(@board) && !tie(@board)
+      if !is_game_over(@board, @size) && !tie(@board)
         eval_board
       end
       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
     end
+
+    
+    # # loop through until the game was won or tied
+    # until game_is_over(@board) || tie(@board)
+    #   get_human_spot
+    #   if !game_is_over(@board) && !tie(@board)
+    #     eval_board
+    #   end
+    #   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+    # end
     
     puts "Game over"
+  end
+
+  def get_game_difficulty
+    # reads 
+    difficulty = nil
+    
+    puts " Choose difficulty level:"
+    puts " 1 - Newbie :(\n 2 - Medium :)\n 3 - Hard >:)"
+
+    until difficulty
+      difficulty = gets.chomp.to_i
+
+      if !difficulty.between?(1,3)
+        difficulty = nil
+      end
+      
+    end
+
+    return difficulty
   end
 
   def is_valid_spot(spot, board)
@@ -68,38 +100,79 @@ class Game
       spot = gets.chomp
 
       # tests if the slot is empty on the board
-      if is_valid_spot
-        @board[spot] = @hum_mark
+      if is_valid_spot(spot, @board)
+        @board[spot.to_i] = @hum_mark
       else
         spot = nil
       end
     end
   end
 
+  def get_available_spaces(board)
+    # reads board and return an array with empty spaces as strings
+    available_spaces = []
+    
+    # traverses board array --> part
+    board.each do |space|
+      # if slot is available
+      if space != @com_mark && space != @hum_mark
+        # appends available spaces
+        available_spaces << space
+      end
+    end
+
+    return available_spaces
+  end
   def eval_board
     spot = nil
+    
     until spot
-
+      
       if @board[4] == "4" # if 4 is empty, computer makes its move there
         spot = 4
         @board[spot] = @com_mark
       else # if 4 is not empty
-        spot = get_best_move(@board, @com_mark)
-        if @board[spot] != "X" && @board[spot] != "O"
+        spot = get_best_move(@board, @com_mark) # gets int with index
+        if @board[spot] != "X" && @board[spot] != "O" 
           # sets best move for pc
           @board[spot] = @com_mark
         else
           spot = nil
         end
       end
+      
     end
   end
 
+  def get_com_move(board, difficulty, random_gen=nil)
+    as = get_available_spaces(board)
+
+    # checks for seed (used for testing)
+    if !random_gen
+      random_gen = Random.new
+    end
+      
+    case difficulty
+    when 1 # newbie
+      # here, PC makes random moves only            
+      move = random_gen.rand(as.first.to_i .. as.last.to_i)
+
+      return move
+    when 2 # medium
+      # PC makes random movies but blocks if user is about to win
+
+      
+      return nil
+    when 3 # hard
+      return nil
+    end
+  end
+  
   def get_best_move(board, next_player, depth = 0, best_score = {})
     available_spaces = [] # available spaces on board
     best_move = nil       # best move
 
-    # traverses board array
+    # traverses board array --> part
     board.each do |s|
       # if slot is available
       if s != "X" && s != "O"
@@ -137,16 +210,107 @@ class Game
     end
   end
 
+  def horizontal_scan(board, n)
+    row_values = []
+    count = 0
+    
+    board.each do |s|      
+      row_values << s
+      count += 1
+
+      # end of the row
+      if count == n
+        if row_values.uniq.length == 1
+          return true
+        else
+          # reset for next row
+          count = 0
+          row_values = []
+        end
+      end
+    end
+
+    return false
+  end
+
+  def vertical_scan(board, n)
+    col_values = []
+    nrow = n
+    i = 0
+    j = 0
+
+    # loops through columns
+    while i < n
+      j = i
+
+      # loops through rows
+      while nrow > 0
+        col_values << board[j]        
+        j += n
+        nrow -= 1
+      end
+      
+      if col_values.uniq.length == 1
+        return true
+      else
+        col_values = []
+        nrow = n
+        i += 1
+      end
+    end
+
+    return false
+  end
+
+  def diag_scan(board, n)
+    diag_values = []
+    count = 0
+    
+    board.each_with_index do |s, i|
+      if i == count
+        diag_values << s
+        count += (n + 1)
+      end      
+    end
+
+    if diag_values.uniq.length == 1
+      return true
+    end
+
+    diag_values = []
+    count = n - 1
+
+    board.each_with_index do |s, i|
+      if i == count
+        diag_values << s
+        count += (n - 1)
+      end
+    end
+
+    diag_values = diag_values[0...-1] 
+    
+    if diag_values.uniq.length == 1
+      return true
+    end
+
+    # no diagonals found
+    return false
+  end
+  
+  def is_game_over(board, n)
+    return vertical_scan(board, n) || horizontal_scan(board, n) || diag_scan(board, n)
+  end
+  
   def game_is_over(b)
-    # checks if uniq return only one marker which means someone has won
-    [b[0], b[1], b[2]].uniq.length == 1 ||
-    [b[3], b[4], b[5]].uniq.length == 1 ||
-    [b[6], b[7], b[8]].uniq.length == 1 ||
-    [b[0], b[3], b[6]].uniq.length == 1 ||
-    [b[1], b[4], b[7]].uniq.length == 1 ||
-    [b[2], b[5], b[8]].uniq.length == 1 ||
-    [b[0], b[4], b[8]].uniq.length == 1 ||
-    [b[2], b[4], b[6]].uniq.length == 1
+    # # checks if uniq return only one marker which means someone has won
+    # [b[0], b[1], b[2]].uniq.length == 1 ||
+    # [b[3], b[4], b[5]].uniq.length == 1 ||
+    # [b[6], b[7], b[8]].uniq.length == 1 ||
+    # [b[0], b[3], b[6]].uniq.length == 1 ||
+    # [b[1], b[4], b[7]].uniq.length == 1 ||
+    # [b[2], b[5], b[8]].uniq.length == 1 ||
+    # [b[0], b[4], b[8]].uniq.length == 1 ||
+    # [b[2], b[4], b[6]].uniq.length == 1
   end
 
   def tie(b)
@@ -156,7 +320,7 @@ class Game
 
 end
 
-TEST = true
+TEST = false
 
 if !TEST
   game = Game.new
